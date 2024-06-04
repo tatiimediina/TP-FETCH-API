6f4c698bd70d7e5a5f0d9ceb005f694cfd58d604
Tatiana Medina <tatymedina284@gmail.com>
2024-06-04 14:15:50 -0300

    modificacion del js y comienzo del css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,28 @@
-async function obtenerPokemon() {
-    const url = `https://pokeapi.co/api/v2/pokemon/`;
-    const respuesta = await fetch(url);
+const contenido = document.getElementById("pokemon-list")
 
-    if (respuesta.ok) {
-      const datosPokemon = await respuesta.json();
-      return datosPokemon;
-    } else {
-      console.error(`Error al obtener Pokémon: ${respuesta.status}`);
-      return null;
-    }
+const ferchtData = async () =>{
+
+  try{
+
+    const pregunta = await fetch("https://rickandmortyapi.com/api/character")
+    const data = await pregunta.json();
+    const respuesta = data.results
+    console.log(respuesta)
+
+  respuesta.forEach(respu => {
+    const item = document.createElement("div")
+    item.className = "pokemon-container"
+    item.innerHTML += `
+    <h3>${respu.name}</h3>
+    <img src="${respu.image}" alt="${respu.name}">
+    `;
+    contenido.appendChild(item)
+  });
+  
+  } catch(error){
+    console.error("hubo un error")
   }
 
-  function mostrarPokemon(datosPokemon) {
-    const listaPokemon = document.getElementById('pokemon-list');
+}
 
-    datosPokemon.results.forEach((pokemon) => {
-      const divPokemon = document.createElement("div");
-      divPokemon.classList.add("pokemon");
-
-      const imgPokemon = document.createElement("img");
-      imgPokemon.src = pokemon.sprites.front_default;
-      imgPokemon.alt = pokemon.name;
-
-      const nombrePokemon = document.createElement("span");
-      nombrePokemon.textContent = pokemon.name;
-
-      divPokemon.appendChild(imgPokemon);
-      divPokemon.appendChild(nombrePokemon);
-      listaPokemon.appendChild(divPokemon);
-    });
-  }
-
-  obtenerPokemon().then(datosPokemon => mostrarPokemon(datosPokemon));
+ferchtData()
